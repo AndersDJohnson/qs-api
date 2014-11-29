d0806c510585d04637090669b7f9893e2badbc2b
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2014-11-29 17:41:24 -0600

    add options and pretty printing support

--- app/index.js
+++ app/index.js
@@ -5,10 +5,38 @@ var url = require('url');
 var unq = require('..');
 
 app.get('/', function (req, res) {
+
+  // TODO: expose option to change options key, in case '_' is used
+  var optsKey = '_';
+
   var query = url.parse(req.url).query;
   var o = unq(query);
+
+  var opts = o[optsKey];
+  delete o[optsKey];
+
+  var opts = makeOpts(opts);
+
+  var settingsBackup = {
+    'json spaces': app.get('json spaces')
+  };
+  // TODO: Is setting safe for concurrency?
+  app.set('json spaces', opts.spaces);
+
   res.json(o);
+
+  app.set('json spaces', settingsBackup('json spaces'));
 });
+
+var defaultOpts = {
+  spaces: 2
+};
+
+var makeOpts = function (param) {
+  param = param || {};
+  param.spaces = param.spaces ? parseInt(param.spaces) : defaultOpts.spaces;
+  return param;
+};
 
 var port = process.env.PORT || 3301;
 app.set('port', port);
